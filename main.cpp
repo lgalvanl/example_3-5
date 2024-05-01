3716fe4a4e24c3d41eb7878f27fcaa50dfcbd6ad
lgalvanl <lgalvanl@fi.uba.ar>
2024-05-01 19:22:21 +0000

    Codigo modificado (falta probar)

--- main.cpp
+++ main.cpp
@@ -17,17 +17,17 @@
 
 DigitalIn enterButton(BUTTON1);
 DigitalIn alarmTestButton(D2);
-DigitalIn aButton(D4);
-DigitalIn bButton(D5);
-DigitalIn cButton(D6);
-DigitalIn dButton(D7);
-DigitalIn mq2(PE_12);
+DigitalIn aButton(D4); // cuarto pin desde arriba
+DigitalIn bButton(D5); //tercer pin desde arriba
+DigitalIn cButton(D6); //segundo pin desde arriba
+DigitalIn dButton(D7); //primer pin desde arriba
+DigitalIn mq2(PE_12);  //quinto pin desde abajo
 
 DigitalOut alarmLed(LED1);
 DigitalOut incorrectCodeLed(LED3);
 DigitalOut systemBlockedLed(LED2);
 
-DigitalInOut sirenPin(PE_10);
+DigitalInOut sirenPin(PE_10); //sexto pin desde abajo
 
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
@@ -39,7 +39,7 @@ AnalogIn lm35(A1);
 bool alarmState    = OFF;
 bool incorrectCode = false;
 bool overTempDetector = OFF;
-
+//pulldown para gas
 int numberOfIncorrectCodes = 0;
 int buttonBeingCompared    = 0;
 int codeSequence[NUMBER_OF_KEYS]   = { 1, 1, 0, 0 }; //Se elige la siguiente secuencia de teclas como clave para apagar la alarma
@@ -54,7 +54,10 @@ float lm35ReadingsAverage  = 0.0;
 float lm35ReadingsSum      = 0.0;
 float lm35ReadingsArray[NUMBER_OF_AVG_SAMPLES];
 float lm35TempC            = 0.0;
+//#define BUS_IN
+#define DIGITAL_IN
 
+#ifdef DIGITAL_IN
 //=====[Declarations (prototypes) of public functions]=========================
 
 void inputsInit();
@@ -80,6 +83,7 @@ int main()
         alarmDeactivationUpdate();
         uartTask();
         delay(TIME_INCREMENT_MS);
+        //printf("%d\n", aButton.read());
     }
 }
 
@@ -92,6 +96,9 @@ void inputsInit()
     bButton.mode(PullDown);
     cButton.mode(PullDown);
     dButton.mode(PullDown);
+    mq2.mode(PullUp); //Como no tenemos el mq2, de esta 
+    //manera nos aseguramos que en el pin se lea un 1 en todo momento
+    //por lo que no se activa la alarma por deteccion de gas
     sirenPin.mode(OpenDrain);
     sirenPin.input();
 }
@@ -368,3 +375,362 @@ float celsiusToFahrenheit( float tempInCelsiusDegrees )
 {
     return ( tempInCelsiusDegrees * 9.0 / 5.0 + 32.0 );
 }
+
+#endif
+
+#ifdef BUS_IN
+DigitalIn enterButton(BUTTON1);
+DigitalIn alarmTestButton(D2);
+BusIn Buttons(PF_13,PE_9,PE_11,PF_14);  //   (1p)
+DigitalIn mq2(PE_12);
+
+BusOut Leds(LED1,LED2,LED3); //  (5p)
+//DigitalOut alarmLed(LED1);
+//DigitalOut incorrectCodeLed(LED3);
+//DigitalOut systemBlockedLed(LED2);
+
+DigitalInOut sirenPin(PE_10);
+
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+AnalogIn potentiometer(A0);
+AnalogIn lm35(A1);
+
+//=====[Declaration and initialization of public global variables]=============
+//Se inicializa la placa con la alarma apagada, sin deteccion de sobretemperatura y con ningun intento de codigo incorrecto
+bool alarmState    = OFF;
+bool incorrectCode = false;
+bool overTempDetector = OFF;
+
+int numberOfIncorrectCodes = 0;
+int buttonBeingCompared    = 0;
+int codeSequence[NUMBER_OF_KEYS]   = { 1, 1, 0, 0 }; //Se elige la siguiente secuencia de teclas como clave para apagar la alarma
+int buttonsPressed[NUMBER_OF_KEYS] = { 0, 0, 0, 0 };
+int accumulatedTimeAlarm = 0;
+
+bool gasDetectorState          = OFF;
+bool overTempDetectorState     = OFF;
+
+float potentiometerReading = 0.0;
+float lm35ReadingsAverage  = 0.0;
+float lm35ReadingsSum      = 0.0;
+float lm35ReadingsArray[NUMBER_OF_AVG_SAMPLES];
+float lm35TempC            = 0.0;
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+void alarmActivationUpdate();
+void alarmDeactivationUpdate();
+
+void uartTask();
+void availableCommands();
+bool areEqual();
+float celsiusToFahrenheit( float tempInCelsiusDegrees );
+float analogReadingScaledWithTheLM35Formula( float analogReading );
+
+//=====[Main function, the program entry point after power on or reset]========
+// En el main se inicializan los puertos de entrada y de salida, en un loop infinito 
+int main()
+{
+    inputsInit(); 
+    outputsInit();
+    while (true) {
+        alarmActivationUpdate();
+        alarmDeactivationUpdate();
+        uartTask();
+        delay(TIME_INCREMENT_MS);
+        printf("El valor del boton A es: %d\n", buttons[3]);
+        printf("El valor del boton B es: %d\n", buttons[2]);
+        printf("El valor del boton C es: %d\n", buttons[1]);
+        printf("El valor del boton D es: %d\n", buttons[0]);
+        printf("El valor de AlarmLed es: %d\n", Leds[0]);
+        printf("El valor de IncorrectCodeLed es: %d\n", Leds[1]);
+        printf("El valor de SystemBlockedCodeLed es: %d\n", Leds[2]);
+    }
+}
+
+//=====[Implementations of public functions]===================================
+//Se configuran los pulsadores como Pull down
+void inputsInit()
+{
+    alarmTestButton.mode(PullDown);
+    Buttons.mode(PullDown); //     (2p)
+    sirenPin.mode(OpenDrain);
+    sirenPin.input();
+}
+//El sistema inicia con la alarma apagada
+void outputsInit()    // (6p)
+{
+    int i;
+
+    for (i = 0; i < 2; i++) {
+        Leds[i] = OFF;
+    }
+}
+//Funcion que analiza a los pines conectados a LM35 y al MQ-2, y dependiendo del caso prende o no a la alarma
+void alarmActivationUpdate() // (7p)
+{
+    static int lm35SampleIndex = 0;
+    int i = 0;
+
+    lm35ReadingsArray[lm35SampleIndex] = lm35.read();
+    lm35SampleIndex++;
+    if ( lm35SampleIndex >= NUMBER_OF_AVG_SAMPLES) {
+        lm35SampleIndex = 0;
+    }
+    
+       lm35ReadingsSum = 0.0;
+    for (i = 0; i < NUMBER_OF_AVG_SAMPLES; i++) {
+        lm35ReadingsSum = lm35ReadingsSum + lm35ReadingsArray[i];
+    }
+    lm35ReadingsAverage = lm35ReadingsSum / NUMBER_OF_AVG_SAMPLES;
+       lm35TempC = analogReadingScaledWithTheLM35Formula ( lm35ReadingsAverage );    
+    
+    if ( lm35TempC > OVER_TEMP_LEVEL ) {
+        overTempDetector = ON;
+    } else {
+        overTempDetector = OFF;
+    }
+
+    if( !mq2) {
+        gasDetectorState = ON;
+        alarmState = ON;
+    }
+    if( overTempDetector ) {
+        overTempDetectorState = ON;
+        alarmState = ON;
+    }
+    if( alarmTestButton ) {             
+        overTempDetectorState = ON;
+        gasDetectorState = ON;
+        alarmState = ON;
+    }    
+    if( alarmState ) { 
+        accumulatedTimeAlarm = accumulatedTimeAlarm + TIME_INCREMENT_MS;
+        sirenPin.output();                                     
+        sirenPin = LOW;                                        
+    
+        if( gasDetectorState && overTempDetectorState ) {
+            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM ) {
+                accumulatedTimeAlarm = 0;
+                Leds[0] = !Leds[0]; // (7p)
+            }
+        } else if( gasDetectorState ) {
+            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_ALARM ) {
+                accumulatedTimeAlarm = 0;
+                Leds[0] = !Leds[0];
+            }
+        } else if ( overTempDetectorState ) {
+            if( accumulatedTimeAlarm >= BLINKING_TIME_OVER_TEMP_ALARM  ) {
+                accumulatedTimeAlarm = 0;
+                Leds[0] = !Leds[0];
+            }
+        }
+    } else{
+        Leds[0] = OFF;
+        gasDetectorState = OFF;
+        overTempDetectorState = OFF;
+        sirenPin.input();                                  
+    }
+}
+//Funcion que maneja la entrada de claves para desactivar a la alarma
+void alarmDeactivationUpdate() //  (3p) (8p)
+{
+    if ( numberOfIncorrectCodes < 5 ) {
+        if (Buttons[3] && Buttons[2] && Buttons[1] && Buttons[0] && !enterButton ) {
+            incorrectCodeLed = OFF;
+        }
+        if ( enterButton && !Leds[1] && alarmState ) {
+            if ( areEqual() ) {
+                alarmState = OFF;
+                numberOfIncorrectCodes = 0;
+            } else {
+                incorrectCodeLed = ON;
+                numberOfIncorrectCodes++;
+            }
+        }
+    } else {
+        Leds[2] = ON;
+    }
+}
+//Funcion donde se maneja la comunicacion en serie con el teclado de la PC, aqui se mostrará el estado
+// de los sensores y se podrá apagar la alarma introduciendo el codigo,
+//Ademas de poder cambiar la clave que desactiva la alarma
+//Todo dependendiendo de la tecla que el usuario presione
+void uartTask()    // (9p)
+{
+    char receivedChar = '\0';
+    char str[100];
+    int stringLength;
+    if( uartUsb.readable() ) {
+        uartUsb.read( &receivedChar, 1 );
+        switch (receivedChar) {
+        case '1':
+            if ( alarmState ) {
+                uartUsb.write( "The alarm is activated\r\n", 24);
+            } else {
+                uartUsb.write( "The alarm is not activated\r\n", 28);
+            }
+            break;
+
+        case '2':
+            if ( !mq2 ) {
+                uartUsb.write( "Gas is being detected\r\n", 22);
+            } else {
+                uartUsb.write( "Gas is not being detected\r\n", 27);
+            }
+            break;
+
+        case '3':
+            if ( overTempDetector ) {
+                uartUsb.write( "Temperature is above the maximum level\r\n", 40);
+            } else {
+                uartUsb.write( "Temperature is below the maximum level\r\n", 40);
+            }
+            break;
+            
+        case '4':
+            uartUsb.write( "Please enter the code sequence.\r\n", 33 );
+            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 ); 
+            uartUsb.write( "finally 'D' button\r\n", 20 );
+            uartUsb.write( "In each case type 1 for pressed or 0 for ", 41 );
+            uartUsb.write( "not pressed\r\n", 13 );
+            uartUsb.write( "For example, for 'A' = pressed, ", 32 );
+            uartUsb.write( "'B' = pressed, 'C' = not pressed, ", 34);
+            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
+            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
+
+            incorrectCode = false;
+
+            for ( buttonBeingCompared = 0;
+                  buttonBeingCompared < NUMBER_OF_KEYS;
+                  buttonBeingCompared++) {
+
+                uartUsb.read( &receivedChar, 1 );
+                uartUsb.write( "*", 1 );
+
+                if ( receivedChar == '1' ) {
+                    if ( codeSequence[buttonBeingCompared] != 1 ) {
+                        incorrectCode = true;
+                    }
+                } else if ( receivedChar == '0' ) {
+                    if ( codeSequence[buttonBeingCompared] != 0 ) {
+                        incorrectCode = true;
+                    }
+                } else {
+                    incorrectCode = true;
+                }
+            }
+
+            if ( incorrectCode == false ) {
+                uartUsb.write( "\r\nThe code is correct\r\n\r\n", 25 );
+                alarmState = OFF;
+                Leds[1]= OFF;  // (9p)
+                numberOfIncorrectCodes = 0;
+            } else {
+                uartUsb.write( "\r\nThe code is incorrect\r\n\r\n", 27 );
+                Leds[1] = ON;
+                numberOfIncorrectCodes++;
+            }                
+            break;
+
+        case '5':
+            uartUsb.write( "Please enter new code sequence\r\n", 32 );
+            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 );
+            uartUsb.write( "finally 'D' button\r\n", 20 );
+            uartUsb.write( "In each case type 1 for pressed or 0 for not ", 45 );
+            uartUsb.write( "pressed\r\n", 9 );
+            uartUsb.write( "For example, for 'A' = pressed, 'B' = pressed,", 46 );
+            uartUsb.write( " 'C' = not pressed,", 19 );
+            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
+            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
+
+            for ( buttonBeingCompared = 0; 
+                  buttonBeingCompared < NUMBER_OF_KEYS; 
+                  buttonBeingCompared++) {
+
+                uartUsb.read( &receivedChar, 1 );
+                uartUsb.write( "*", 1 );
+
+                if ( receivedChar == '1' ) {
+                    codeSequence[buttonBeingCompared] = 1;
+                } else if ( receivedChar == '0' ) {
+                    codeSequence[buttonBeingCompared] = 0;
+                }
+            }
+
+            uartUsb.write( "\r\nNew code generated\r\n\r\n", 24 );
+            break;
+ 
+        case 'p':
+        case 'P':
+            potentiometerReading = potentiometer.read();
+            sprintf ( str, "Potentiometer: %.2f\r\n", potentiometerReading );
+            stringLength = strlen(str);
+            uartUsb.write( str, stringLength );
+            break;
+
+        case 'c':
+        case 'C':
+            sprintf ( str, "Temperature: %.2f \xB0 C\r\n", lm35TempC );
+            stringLength = strlen(str);
+            uartUsb.write( str, stringLength );
+            break;
+
+        case 'f':
+        case 'F':
+            sprintf ( str, "Temperature: %.2f \xB0 F\r\n", 
+                celsiusToFahrenheit( lm35TempC ) );
+            stringLength = strlen(str);
+            uartUsb.write( str, stringLength );
+            break;
+
+        default:
+            availableCommands();
+            break;
+
+        }
+    }
+}
+//Funcion que muestra por consola todas las opciones que tiene el usuario
+
+void availableCommands()
+{
+    uartUsb.write( "Available commands:\r\n", 21 );
+    uartUsb.write( "Press '1' to get the alarm state\r\n", 34 );
+    uartUsb.write( "Press '2' to get the gas detector state\r\n", 41 );
+    uartUsb.write( "Press '3' to get the over temperature detector state\r\n", 54 );
+    uartUsb.write( "Press '4' to enter the code sequence\r\n", 38 );
+    uartUsb.write( "Press '5' to enter a new code\r\n", 31 );
+    uartUsb.write( "Press 'P' or 'p' to get potentiometer reading\r\n", 47 );
+    uartUsb.write( "Press 'f' or 'F' to get lm35 reading in Fahrenheit\r\n", 52 );
+    uartUsb.write( "Press 'c' or 'C' to get lm35 reading in Celsius\r\n\r\n", 51 );
+}
+//Funcion para ver si el codigo entrante es la clave correcta para apagar la alarma
+bool areEqual()
+{
+    int i;
+
+    for (i = 0; i < NUMBER_OF_KEYS; i++) {
+        if (codeSequence[i] != Buttons[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+//Funciones para convertir el valor leido del potenciometro en unidades Grados o Farenheit
+float analogReadingScaledWithTheLM35Formula( float analogReading )
+{
+    return ( analogReading * 3.3 / 0.01 );
+}
+
+float celsiusToFahrenheit( float tempInCelsiusDegrees )
+{
+    return ( tempInCelsiusDegrees * 9.0 / 5.0 + 32.0 );
+}
+
+#endif
